28b75651163c945fee7374bbc1611ca3e9df17e1
manemis <manemis@gmail.com>
2022-05-16 22:20:38 +0900

    Update 013.2588_곱셈.js

--- code/001.입출력과_사칙연산/013.2588_곱셈.js
+++ code/001.입출력과_사칙연산/013.2588_곱셈.js
@@ -12,3 +12,24 @@
 출력
 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
  */
+
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin", "utf8").split(`\n`);
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+const bString = input[1];
+const bNumber1 = bString.split("")[2];
+const bNumber2 = bString.split("")[1];
+const bNumber3 = bString.split("")[0];
+
+const answer3 = a * bNumber1;
+const answer4 = a * bNumber2;
+const answer5 = a * bNumber3;
+const answer6 = a * b;
+
+console.log(answer3);
+console.log(answer4);
+console.log(answer5);
+console.log(answer6);
